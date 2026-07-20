5ab3b7022def96310efd75eaa7db81b4187adabe
Adeel Khan <adiiiiikhan1998@outlook.com>
2026-07-20 17:33:44 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // Update `base` to your GitHub repo name when deploying to GitHub Pages.
-// Example: base: '/adeel-portfolio/'
 export default defineConfig({
-  base: '/adeel-portfolio/',
+  base: '/hamza-portfolio/',
   plugins: [react(), tailwindcss()],
 })
